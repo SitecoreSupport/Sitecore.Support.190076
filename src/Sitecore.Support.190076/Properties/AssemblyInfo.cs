57c5318367b8882cc403a38e2167c70edfbf08b7
DK\IEKA <ieka@sitecore.net>
2017-10-23 16:01:04 +0300

    initial commit

---
+++ src/Sitecore.Support.190076/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.190076")]
+[assembly: AssemblyProduct("Sitecore.Support.190076")]
+[assembly: ComVisible(false)]
